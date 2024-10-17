909a15a5f72eaef34f580788008240fa2598b430
KafuiAvevor <kafuiavevor@gmail.com>
2024-10-17 01:03:10 +0100

    Update yf.py

--- pages/yf.py
+++ pages/yf.py
@@ -30,7 +30,7 @@ sel_dt1 = cols[0].date_input('Start Date', value=dt.datetime(2024,1,1), format='
 sel_dt2 = cols[1].date_input('End Date', format='YYYY-MM-DD')
 
 if len(sel_tickers) != 0:
-    yfdata = yf.download(list(sel_tickers_list), start=sel_dt1, end=sel_st2)['Close'].reset_index().melt(id_vars = ['Date'], var_name = 'ticker', value_name='price')
+    yfdata = yf.download(list(sel_tickers_list), start=sel_dt1, end=sel_dt2)['Close'].reset_index().melt(id_vars=['Date'], var_name='ticker', value_name='price')
     yfdata['price_start'] = yfdata.groupby('ticker').price.transform('first')
     yfdata['price_pct_daily'] = yfdata.groupby('ticker').price.pct_change()
     yfdata['price_pct'] = (yfdata.price - yfdata.price_start)/ yfdata.price_start
