e18942cc0e1c5a0326a6c6623a9d01a6a7106557
KafuiAvevor <kafuiavevor@gmail.com>
2024-11-15 21:30:28 +0000

    Update BSM.py

--- BSM.py
+++ BSM.py
@@ -42,7 +42,8 @@ with st.sidebar:
         st.session_state.strike_price = col1.number_input("Strike Price ($)", min_value=0.00, value=st.session_state.strike_price, step=0.1, help="Strike price of the option")
         st.session_state.risk_free_rate = col1.number_input("Risk Free Rate (%)", min_value=0.00, value=st.session_state.risk_free_rate, step=0.1, help="Annual risk-free interest rate in percentage (e.g., 5 for 5%)")
         st.session_state.maturity_date = col2.date_input("Maturity Date", min_value = dt.today(), value=st.session_state.maturity_date, help="Date at which the option matures")
-        st.session_state.time_to_expiry = (np.busday_count(str(dt.today().date()), str(st.session_state.maturity_date)))/252
+        business_days_to_expiry = np.busday_count(str(dt.today().date()), str(st.session_state.maturity_date))
+        st.session_state.time_to_expiry = business_days_to_expiry / 252
         st.session_state.volatility = col2.number_input("Volatility (%)", min_value=0.00, value=st.session_state.volatility, step=0.1, help="Annualised volatility in percentage (e.g., 20 for 20%)")
     else:
         st.write("#### Fetch Live Data")
