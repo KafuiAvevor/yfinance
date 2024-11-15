4c1ed2dfba416f3a1189e9946b3505a44312ce2a
KafuiAvevor <kafuiavevor@gmail.com>
2024-11-15 21:37:17 +0000

    Update BSM.py

--- BSM.py
+++ BSM.py
@@ -31,6 +31,8 @@ with st.sidebar:
     if 'maturity_date' not in st.session_state:
         st.session_state.maturity_date = dt.today() + td(weeks=1)
     if 'time_to_expiry' not in st.session_state: #change to date time = expiry date - current date - weekends.
+        today_str = str(dt.today().date())
+        maturity_str = str(st.session_state.maturity_date)
         business_days_to_expiry = np.busday_count(today_str, maturity_str)
         st.session_state.time_to_expiry = business_days_to_expiry / 252
     if 'volatility' not in st.session_state:
