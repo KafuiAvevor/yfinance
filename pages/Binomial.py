99be3fde6d2afbfe35a188eda4035030cf08288c
KafuiAvevor <kafuiavevor@gmail.com>
2024-11-19 16:56:38 +0000

    Update Binomial.py

--- pages/Binomial.py
+++ pages/Binomial.py
@@ -113,15 +113,7 @@ with st.sidebar:
                 st.write("#### Last 5 Days of Closing Prices")
                 st.dataframe(live_data['historical_prices'].tail())
 
-                # Allow user to input or adjust other parameters
-                col1, col2 = st.columns(2)
-                st.session_state.strike_price = col1.number_input("Strike Price ($)", min_value=0.00, value=st.session_state.spot_price, step=0.1, help="Strike price of the option")
-                st.session_state.risk_free_rate = col1.number_input("Risk Free Rate (%)", min_value=0.00, value=st.session_state.risk_free_rate, step=0.1, help="Annual risk-free interest rate in percentage (e.g., 5 for 5%)")
-                st.session_state.maturity_date = col2.date_input("Maturity Date", min_value = dt.today(), value=st.session_state.maturity_date, help="Date at which the option matures")
-                today_str = str(dt.today().date())
-                maturity_str = str(st.session_state.maturity_date)
-                business_days_to_expiry = pd.bdate_range(today_str, maturity_str).size
-                st.session_state.time_to_expiry = business_days_to_expiry / 252
+
     st.markdown("---")
     st.header("Heatmap Parameters")
     col1, col2 = st.columns(2)
