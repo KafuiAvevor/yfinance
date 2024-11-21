4a51a59fa9fe7b3073a444a3f6d4286635801804
KafuiAvevor <kafuiavevor@gmail.com>
2024-11-21 16:32:28 +0000

    Update Binomial.py

--- pages/Binomial.py
+++ pages/Binomial.py
@@ -18,15 +18,9 @@ st.markdown("### By Kafui Avevor")
 
 
 # Sidebar Inputs
-import streamlit as st
-import yfinance as yf
-import numpy as np
-from scipy.stats import norm
-
-# Sidebar Inputs
 with st.sidebar:
     st.write("### Data Input Method")
-    data_input_method = st.radio("Choose Data Input Method", ("Manual Input", "Live Data from Yahoo Finance"))
+    data_input_method = st.radio( ("Live Data from Yahoo Finance"))
 
     # Initialise session state for parameters if not already set
     if 'spot_price' not in st.session_state:
@@ -57,21 +51,7 @@ with st.sidebar:
     if 'currency' not in st.session_state:
         st.session_state.currency = 'USD'
 
-    if data_input_method == "Manual Input":
-        st.write("#### Manual Input Parameters")
-        col1, col2 = st.columns(2)
-        st.session_state.spot_price = col1.number_input("Spot Price ($)", min_value=0.00, value=st.session_state.spot_price, step=0.1, help="Current price of the underlying asset")
-        st.session_state.strike_price = col1.number_input("Strike Price ($)", min_value=0.00, value=st.session_state.spot_price, step=0.1, help="Strike price of the option")
-        st.session_state.risk_free_rate = col1.number_input("Risk Free Rate (%)", min_value=0.00, value=st.session_state.risk_free_rate, step=0.1, help="Annual risk-free interest rate in percentage (e.g., 5 for 5%)")
-        st.session_state.maturity_date = col2.date_input("Maturity Date", min_value = dt.today(), value=st.session_state.maturity_date, help="Date at which the option matures")
-        today_str = str(dt.today().date())
-        maturity_str = str(st.session_state.maturity_date)
-        business_days_to_expiry = pd.bdate_range(today_str, maturity_str).size
-        st.session_state.time_to_expiry = business_days_to_expiry / 252
-        st.session_state.volatility = col2.number_input("Volatility (%)", min_value=0.00, value=st.session_state.volatility, step=0.1, help="Annualised volatility in percentage (e.g., 20 for 20%)")
-        st.session_state.number_of_steps = col2.number_input("Number of Steps", min_value=1, value=st.session_state.number_of_steps, step=1, help="Number of steps")
-   
-    else:
+    if data_input_method == "Live Data from Yahoo Finance":
         st.write("#### Fetch Live Data")
         ticker = st.text_input("Enter Stock Ticker", value="AAPL", help="Enter the ticker symbol (e.g., AAPL, MSFT, GOOG)")
         fetch_expirations = st.button("Fetch Available Maturities")
