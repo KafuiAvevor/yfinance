842542383bd554b18a503ba1c79d6f631edec765
KafuiAvevor <kafuiavevor@gmail.com>
2024-11-24 19:26:59 +0000

    Update Binomial.py

--- pages/Binomial.py
+++ pages/Binomial.py
@@ -126,7 +126,8 @@ with st.sidebar:
                 stock = yf.Ticker(ticker)
                 currency = stock.info['currency']
                 hist = stock.history(period="1y")  # 1 year of historical data
-                current_price = hist['Close'][-1]
+                close_price = hist['Close'][-1]
+                current_price = stock.fast_info["last_price"]
                 options = stock.option_chain(maturity_date)
                 puts_volume = options.puts['volume'].sum()
                 call_volume = options.calls['volume'].sum()
