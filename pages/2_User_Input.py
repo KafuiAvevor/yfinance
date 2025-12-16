Fix calculation of d1 in black_scholes function

pages/2_User_Input.py
@@ -22,7 +22,7 @@ with st.sidebar:
     volatility = col2.number_input("Volatility", min_value=0.00, value=20.00)
 
 def black_scholes(spot_price, strike_price, risk_free_rate, time_to_expiry, volatility, option_type="call"):
-    d1 = (np.log(spot_price / strike_price) + ((risk_free_rate/100) + (volatility/100)**2 / 2) * time_to_expiry) / (volatility * np.sqrt(time_to_expiry))
+    d1 = (np.log(spot_price / strike_price) + ((risk_free_rate/100) + (volatility/100)**2 / 2) * time_to_expiry) / ((volatility/100) * np.sqrt(time_to_expiry))
     d2 = d1 - (volatility/100) * np.sqrt(time_to_expiry)
     if option_type == "call":
         price = spot_price * norm.cdf(d1) - strike_price * np.exp(-risk_free_rate * time_to_expiry) * norm.cdf(d2)